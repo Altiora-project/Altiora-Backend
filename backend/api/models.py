3d8ae16d64130b9aaa3d503c3c793029a6bf2191
Alexandr <khsashko@yandex.ru>
2025-07-16 18:47:55 +0300

    Add model Partner

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,29 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Partner(models.Model):
+    """Модель партнёра."""
+
+    name = models.CharField(
+        verbose_name="Название партнёра",
+        max_length=100,
+        unique=True,
+    )
+    logo = models.ImageField(
+        verbose_name="Логотип",
+        upload_to="partners",
+        blank=True,
+        null=True
+    )
+    website = models.URLField(
+        verbose_name="Сайт партнёра",
+        blank=True
+    )
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "Партнёр"
+        verbose_name_plural = "Партнёры"
+
+    def __str__(self):
+        return self.name[:20]
