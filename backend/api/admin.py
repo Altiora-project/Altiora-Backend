3d94cf73194936454bc0630a2a5fa7a3c982e02a
Alexandr <khsashko@yandex.ru>
2025-07-20 21:19:27 +0300

    удалил модели

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,16 +1,1 @@
-from django.contrib import admin
-from django.utils.safestring import mark_safe
 
-from .models import Partner
-
-
-@admin.register(Partner)
-class PartnerAdmin(admin.ModelAdmin):
-    """Админка для модели парнёр."""
-
-    list_display = ("name", "website", "get_logo")
-
-    @admin.display(description='Лого')
-    def get_logo(self, obj):
-        """Возвращает лого."""
-        return mark_safe(f"<img src={obj.image.url} width='80' height='60'>")
