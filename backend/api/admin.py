b4081fddde663f316b74378c3cfb3019b70900d5
Sergey Korobok <korvin@kati.ru>
2025-07-20 15:57:56 +0300

    feat: add service model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,7 +3,7 @@ from typing import Optional
 from django.contrib import admin
 from django.http import HttpRequest
 
-from .models import ProjectRequest, Technology
+from .models import ProjectRequest, Technology, ServicePostscriptum, Service, ServiceContent, Tag, CaseStudy
 
 
 @admin.register(ProjectRequest)
@@ -67,3 +67,144 @@ class TechnologyAdmin(admin.ModelAdmin):
     )
     search_fields = ('name', )
     list_filter = ('number', 'name')
+
+
+@admin.register(ServicePostscriptum)
+class ServicePostscriptumAdmin(admin.ModelAdmin):
+    """Админка для управления постскриптумами услуг"""
+
+    list_display = [
+        'id',
+        'name',
+        'info',
+        'item1',
+        'item2',
+        'item3',
+        'item4',
+    ]
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'info']
+    list_filter = ['name']
+    ordering = ['name']
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'info')
+        }),
+        ('Элементы постскриптума', {
+            'fields': ('item1', 'item2', 'item3', 'item4'),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    """Админка для управления услугами"""
+
+    list_display = [
+        'id',
+        'number',
+        'name',
+        'info',
+        'contents_count',
+        'tags_display'
+    ]
+    list_display_links = ['id', 'number', 'name']
+    search_fields = ['name', 'info', 'tags__name']
+    list_filter = ['number', 'tags']
+    ordering = ['number']
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('number', 'name', 'info')
+        }),
+        ('Теги', {
+            'fields': ('tags',),
+            'classes': ('collapse',)
+        }),
+    )
+
+    def contents_count(self, obj):
+        """Количество составов услуги"""
+        return obj.contents.count()
+    contents_count.short_description = "Количество составов"
+
+    def tags_display(self, obj):
+        """Отображение тегов в списке"""
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    tags_display.short_description = "Теги"
+
+
+@admin.register(ServiceContent)
+class ServiceContentAdmin(admin.ModelAdmin):
+    """Админка для управления составами услуг"""
+
+    list_display = [
+        'id',
+        'service',
+        'number',
+        'name',
+        'info'
+    ]
+    list_display_links = ['id', 'number', 'name']
+    search_fields = ['name', 'info', 'service__name']
+    list_filter = ['service', 'number']
+    ordering = ['service', 'number']
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('service', 'number', 'name', 'info')
+        }),
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """Админка для управления тегами"""
+
+    list_display = [
+        'id',
+        'name'
+    ]
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    ordering = ['name']
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name',)
+        }),
+    )
+
+
+@admin.register(CaseStudy)
+class CaseStudyAdmin(admin.ModelAdmin):
+    """Админка для управления реальными проектами"""
+
+    list_display = [
+        'id',
+        'service',
+        'name',
+        'info',
+        'tags_display'
+    ]
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'info', 'service__name', 'tags__name']
+    list_filter = ['service', 'tags']
+    ordering = ['service', 'name']
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('service', 'name', 'info')
+        }),
+        ('Теги', {
+            'fields': ('tags',),
+            'classes': ('collapse',)
+        }),
+    )
+
+    def tags_display(self, obj):
+        """Отображение тегов в списке"""
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    tags_display.short_description = "Теги"
--- backend/api/models.py
+++ backend/api/models.py
@@ -74,3 +74,150 @@ class Technology(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ServicePostscriptum(models.Model):
+    """Модель для постскриптума услуги"""
+
+    name = models.CharField(
+        max_length=250,
+        verbose_name="Название"
+    )
+    info = models.TextField(
+        max_length=1000,
+        verbose_name="Информация"
+    )
+    item1 = models.CharField(
+        max_length=250,
+        verbose_name="Элемент 1"
+    )
+    item2 = models.CharField(
+        max_length=250,
+        verbose_name="Элемент 2"
+    )
+    item3 = models.CharField(
+        max_length=250,
+        verbose_name="Элемент 3"
+    )
+    item4 = models.CharField(
+        max_length=250,
+        verbose_name="Элемент 4"
+    )
+
+    class Meta:
+        verbose_name = "Постскриптум услуги"
+        verbose_name_plural = "Постскриптумы услуг"
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """Модель для тегов"""
+
+    name = models.CharField(
+        max_length=250,
+        verbose_name="Название тега"
+    )
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    """Модель для услуг"""
+
+    name = models.CharField(
+        max_length=250,
+        verbose_name="Название услуги"
+    )
+    number = models.PositiveSmallIntegerField(
+        verbose_name="№ п/п в списке услуг"
+    )
+    info = models.TextField(
+        max_length=1000,
+        verbose_name="Информация об услуге"
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        related_name='services',
+        verbose_name="Теги"
+    )
+
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+        ordering = ['number']
+
+    def __str__(self):
+        return f"{self.number}. {self.name}"
+
+
+class ServiceContent(models.Model):
+    """Модель для состава услуги"""
+
+    number = models.PositiveSmallIntegerField(
+        verbose_name="№ п/п в составе услуги"
+    )
+    service = models.ForeignKey(
+        Service,
+        on_delete=models.CASCADE,
+        related_name='contents',
+        verbose_name="Услуга"
+    )
+    name = models.CharField(
+        max_length=250,
+        verbose_name="Название состава"
+    )
+    info = models.TextField(
+        max_length=1000,
+        verbose_name="Информация о составе"
+    )
+
+    class Meta:
+        verbose_name = "Состав услуги"
+        verbose_name_plural = "Составы услуг"
+        ordering = ['service', 'number']
+        unique_together = ['service', 'number']
+
+    def __str__(self):
+        return f"{self.service.name} - {self.number}. {self.name}"
+
+
+class CaseStudy(models.Model):
+    """Модель для реальных проектов"""
+
+    service = models.ForeignKey(
+        Service,
+        on_delete=models.CASCADE,
+        related_name='case_studies',
+        verbose_name="Услуга"
+    )
+    name = models.CharField(
+        max_length=250,
+        verbose_name="Название проекта"
+    )
+    info = models.TextField(
+        max_length=1000,
+        verbose_name="Информация о проекте"
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        related_name='case_studies',
+        verbose_name="Теги"
+    )
+
+    class Meta:
+        verbose_name = "Реальный проект"
+        verbose_name_plural = "Реальные проекты"
+        ordering = ['service', 'name']
+
+    def __str__(self):
+        return f"{self.service.name} - {self.name}"
