d73ae689cd3943c290a2266ef4103959ff020c89
Alexandr <khsashko@yandex.ru>
2025-07-22 09:40:02 +0300

    проверенно flake8 и black

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -35,11 +35,6 @@ class ProjectRequestAdmin(admin.ModelAdmin):
         """Запрет добавления заявок"""
         return False
 
-
-@admin.register(Partner)
-class PartnerAdmin(admin.ModelAdmin):
-    """Админка для модели парнёр."""
-
     def has_change_permission(
         self, request: HttpRequest, obj: Optional[ProjectRequest] = None
     ) -> bool:
@@ -53,6 +48,18 @@ class PartnerAdmin(admin.ModelAdmin):
         return False
 
 
+@admin.register(Partner)
+class PartnerAdmin(admin.ModelAdmin):
+    """Админка для модели парнёр."""
+
+    list_display = ("name", "website", "get_logo")
+
+    @admin.display(description="Лого")
+    def get_logo(self, obj):
+        """Возвращает лого."""
+        return mark_safe(f"<img src={obj.image.url} width='80' height='60'>")
+
+
 @admin.register(Technology)
 class TechnologyAdmin(admin.ModelAdmin):
     """Модель описывает управление технологиями блока Лаборатория стартапов."""
@@ -61,9 +68,3 @@ class TechnologyAdmin(admin.ModelAdmin):
     list_display = ("name", "number", "primary_info", "secondary_info")
     search_fields = ("name",)
     list_filter = ("number", "name")
-    list_display = ("name", "website", "get_logo")
-
-    @admin.display(description="Лого")
-    def get_logo(self, obj):
-        """Возвращает лого."""
-        return mark_safe(f"<img src={obj.image.url} width='80' height='60'>")
